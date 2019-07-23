3543fae6e70269870d25f87c165b1255ccbe1e8b
Jason Utt <jdmagic21@gmail.com>
2019-07-23 19:33:40 -0400

    Create video.ts

---
+++ src/video.ts
@@ -0,0 +1,36 @@
+import * as youtubedl from 'youtube-dl'; 
+import * as path from 'path'; 
+import * as fs from 'fs'; 
+
+interface format{
+    format:string,
+    fileSize: string,
+    ext: string
+}
+
+class video{
+    url:string;
+    constructor(url){
+        this.url = url; 
+    }
+    private GetFormats(){
+        youtubedl.getInfo(this.url, (err, info)=>{
+            if (err) throw err; 
+            let videoFormats = info.formats.map((value)=>{
+                return {
+                    format: value.format,
+                    filesize: value.filesize,
+                    ext: value.ext
+                }
+            });
+            return videoFormats;
+        })
+    }
+    private download(formatCode, outputName){
+        var video = youtubedl(this.url, [`--format=${formatCode}`],{cwd: __dirname}); 
+        video.pipe(fs.createWriteStream(outputName)); 
+        video.on('end',()=>{
+            console.log('video is done');
+        });
+    }
+}
