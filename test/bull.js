e830d75aa87838a2e3988687c1cd0b0c6e009193
Jason Utt <jdmagic21@gmail.com>
2019-08-13 17:16:44 -0400

    bull

---
+++ test/bull.js
@@ -0,0 +1,14 @@
+import * as bull from 'bull';
+
+describe('Learn how to use Bull.js', ()=>{
+    it('should create a new queue', ()=>{
+        var simpleQue = new Queue('simpleMath', '' )
+    }); 
+    it('should create 2 new job consumers on a queue', ()=>{
+
+    }); 
+    it('should return listen to finished jobs on a queue', ()=>{
+
+    }); 
+
+});
